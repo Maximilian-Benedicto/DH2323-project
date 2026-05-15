2379580949f2516f2436aebbe9fc19f2d366ac6a
Maximilian Benedicto <maximilian.benedicto@live.se>
2026-05-15 18:13:15 +0200

    Refactor code structure for improved readability and maintainability

---
+++ ProjectSubmission.sh
@@ -0,0 +1,5 @@
+rm -rf ProjectSubmission
+mkdir -p ProjectSubmission
+zip -r ProjectSubmission/Code.zip src model img README.md
+cp report/ProjectReport.pdf ProjectSubmission/
+cp report/ProjectSpecification.pdf ProjectSubmission/
